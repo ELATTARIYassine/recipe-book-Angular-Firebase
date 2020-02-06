a00e59df3817d4c7d8ccdae852f5b332536dd2b6
El attari Yassine <yassineattari64@gmail.com>
2020-02-06 13:40:50 +0100

    [evt binding] Passing Data with Event and Property Binding (Combined)

--- src/app/recipes/recipe-list/recipes-list.component.ts
+++ src/app/recipes/recipe-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,14 +8,22 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipeStaticImg = "https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto,w_610/v1/hellofresh_s3/image/5e25f8a6e5491a4d4a6f9912-87bb0d1e.jpg"
+
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simple test', '../../../assets/recipeImg.jpg'),
-    new Recipe('A test recipe', 'This is simple test', '../../../assets/recipeImg.jpg') 
+    new Recipe('Indian food', 'This is simple test', this.recipeStaticImg),
+    new Recipe('Burger', 'This is simple test', this.recipeStaticImg) 
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
