1490b137e506d8afd482fda0cfce32a2554dcdda
El attari Yassine <yassineattari64@gmail.com>
2020-02-07 04:27:52 +0100

    [Services] Using a Service for Cross-Component Communication for the Selected Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,11 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
 
-    recipeStaticImg = "https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto,w_610/v1/hellofresh_s3/image/5e25f8a6e5491a4d4a6f9912-87bb0d1e.jpg"
-
+    recipeStaticImg = "https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto,w_610/v1/hellofresh_s3/image/5e25f8a6e5491a4d4a6f9912-87bb0d1e.jpg";
+    recipeSelected = new EventEmitter<Recipe>();
+    
     private recipes: Recipe[] = [
         new Recipe('Indian food', 'This is simple test', this.recipeStaticImg),
         new Recipe('Burger', 'This is simple test', this.recipeStaticImg) 
