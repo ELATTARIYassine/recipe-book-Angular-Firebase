26c2843de3f25edf27a0eebc2b60801c0bdb0f59
El attari Yassine <yassineattari64@gmail.com>
2020-05-23 16:08:49 +0000

    [frontend] Added firebase setup  & setting up storing service and resolver

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,18 +14,21 @@ export class RecipeService {
     recipeStaticImg = "https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto,w_610/v1/hellofresh_s3/image/5e25f8a6e5491a4d4a6f9912-87bb0d1e.jpg";
     recipeSelected = new EventEmitter<Recipe>();
     
-    private recipes: Recipe[] = [
-        new Recipe('Indian food', 'This is simple test', this.recipeStaticImg, 
-        [
-            new Ingredient('Meat', 1), 
-            new Ingredient('French Fries', 10)
-        ]),
-        new Recipe('Burger', 'This is simple test', this.recipeStaticImg, 
-        [
-            new Ingredient('Buns', 2),
-            new Ingredient('Meat', 1)
-        ]) 
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Indian food', 'This is simple test', this.recipeStaticImg, 
+    //     [
+    //         new Ingredient('Meat', 1), 
+    //         new Ingredient('French Fries', 10)
+    //     ]),
+    //     new Recipe('Burger', 'This is simple test', this.recipeStaticImg, 
+    //     [
+    //         new Ingredient('Buns', 2),
+    //         new Ingredient('Meat', 1)
+    //     ]) 
+    // ];
+
+    private recipes: Recipe[] = [];
+
     getRecipes() :  Recipe[]{
         return this.recipes.slice();
     }
@@ -53,6 +56,11 @@ export class RecipeService {
         console.log(this.recipes);
         this.recipes.splice(index, 1);
         console.log(this.recipes);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
         this.recipesChanged.next(this.recipes.slice());
     }
 }
